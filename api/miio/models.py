96498289ea65154bfaf01ae263f992034d3e2e9d
Jason Benn <jasoncbenn@gmail.com>
2020-02-03 16:38:25 -0800

    created_at field on Card.

--- api/miio/models.py
+++ api/miio/models.py
@@ -1,13 +1,16 @@
 from django.contrib.postgres.fields import JSONField
-from django.db import models
+from django.utils import timezone
 from django.db.models import BooleanField
+from django.db.models import DateTimeField
+from django.db.models import Model
 from django.db.models import TextField
 from django_enumfield import enum
 
 from miio.data_types import CardType
 
 
-class Card(models.Model):
+class Card(Model):
+    created_at = DateTimeField(default=timezone.now, editable=False)
     title = TextField(null=True)
     body = TextField(null=True)
     is_archived = BooleanField(default=False)
